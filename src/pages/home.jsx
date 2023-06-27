49fabd913596b29bea5c9240bfbacbc459b0f349
PBV1985 <126014706+PBV1985@users.noreply.github.com>
2023-06-27 17:18:05 +0200

    test2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,30 @@
 
-import ContainerComp from '../components/ContainerComp';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CarouselComp from '../components/CarouselComp';
+import DropdownComp from '../components/DropdownComp';
+import CardComp from '../components/CardComp';
+import CardImgComp from '../components/CardImgComp';
 
 export const Home = () =>{
     return (<div className='ContainerComp'>
-    <ContainerComp/>  
+     <Container className='ContainerComp'>
+      <Row className='section1'>
+        <Col className='CarouselDropdown' sm={6}>
+            <div><CarouselComp/></div><DropdownComp/></Col>
+        <Col className='text' sm={5}><CardComp/></Col>
+      </Row>
+      <Row className='firstLine'>
+        <Col sm><CardImgComp/></Col>
+      </Row>
+      <Row className='secondLine'>
+        <Col sm><CardImgComp/></Col>
+      </Row>
+      <Row className='thirdLine'>
+        <Col sm><CardImgComp/></Col>
+      </Row>
+    </Container>
     </div>   
     );
 }
