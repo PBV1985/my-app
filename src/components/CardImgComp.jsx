67b7b36428ea1d154dcc20a2f6dbc6da4c7a5a57
PBV1985 <126014706+PBV1985@users.noreply.github.com>
2023-07-06 21:00:24 +0200

    test3

--- src/components/CardImgComp.jsx
+++ src/components/CardImgComp.jsx
@@ -29,8 +29,14 @@ movies.map((movie) => {
  return(
   <Col className='col-sm d-flex' key={movie.id}>
     <Card className=''>
-      <Card.Img  variant ="top" src={`${imgURL}${movie.poster_path}`} alt='movie'/>
-    
+      <Card.Img variant="top" src={`${imgURL}${movie.poster_path}`} alt='movie'/>
+      <Card.Body>
+        <Card.Title className='col-sm d-flex shadow-lg bg-body-tertiary rounded'>{movie.title}</Card.Title>
+        
+        <Card.Text className='align-items-start col-9'>
+        {movie.release_date}
+        </Card.Text>
+      </Card.Body>
     </Card>
   </Col>
 );
