da70b32a0bc596f4ce8463260ca07705ce13ec11
PBV1985 <126014706+PBV1985@users.noreply.github.com>
2023-07-17 10:52:40 +0200

    moviePage

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,33 +1,81 @@
+import React, {useState, setState} from "react";
+import {database} from '../firebase'
+import {ref,push,child,update} from "firebase/database";
+
+
+
 export const SignUp = () => {
+    const [Name, setName] = useState(null);
+    const [Email, setEmail] = useState(null);
+    const [Username, setUsername] = useState(null);
+    const [Password, setPassword] = useState(null);
+    
+    const handleInputChange = (e) => {
+        const {id, value} = e.target;
+        if(id === "Name"){
+            setName(value);
+        }
+        if(id === "Email"){
+            setEmail(value);
+        }
+        if(id === "Username"){
+            setUsername(value);
+        }
+        if(id === "Password"){
+            setPassword(value);
+        }
+    }
+
+    const handleSubmit = () => {
+
+        console.log(Name, Email,Username, Password);
+    //     let obj = {
+    //         Name : Name,
+    //         Email:Email,
+    //         Username:Username,
+    //         Password:Password,
+            
+    //     }       
+    // const newPostKey = push(child(ref(database), 'posts')).key;
+    // const updates = {};
+    // updates['/' + newPostKey] = obj
+    // return update(ref(database), updates);
+
+    }
+
+
+
+
+
     return ( 
         <div className="box">
-        <span class="borderLine"></span>
+        <span className="borderLine"></span>
         <form>
             <h2>Sign up</h2>
             <div className="inputBox">
-                <input type="text" required="required"/>
+                <input className="form_input" type="text" id="Name" value={Name} onChange={(e) =>handleInputChange(e)} required="required"/>
                 <span>Name</span>
                 <i></i>
             </div>
             <div className="inputBox">
-                <input type="text" required="required"/>
+                <input className="form_input" type="email" id="Email" value={Email} onChange={(e) =>handleInputChange(e)} required="required"/>
                 <span>Email</span>
                 <i></i>
             </div>
             <div className="inputBox">
-                <input type="text" required="required"/>
+                <input className="form_input" type="text" id="Username" value={Username} onChange={(e) =>handleInputChange(e)} required="required"/>
                 <span>Username</span>
                 <i></i>
             </div>
             <div className="inputBox">
-                <input type="password" required="required"/>
-                <span>Pasword</span>
+                <input className="form_input" type="password" id="Password" value={Password} onChange={(e) =>handleInputChange(e)} required="required"/>
+                <span>Password</span>
                 <i></i>
             </div>
             <div className="links">
-                <a href="#">Login</a>
+                <a href="">Login</a>
             </div>
-            <input type="submit" value="Sign up"/>
+            < button onClick={()=>handleSubmit()} type="submit"  value="Sign up"/>
         </form>
     </div>   
     );
